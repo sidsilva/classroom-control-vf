8d3ff2d28fe9d1dfe86849d6e617be2a9042e82b
sidsilva <sidmikesilva@gmail.com>
2016-05-24 11:49:18 -0600

    Update init.pp

--- site/users/manifests/init.pp
+++ site/users/manifests/init.pp
@@ -1,7 +1,15 @@
 class users {
   user { 'fundamentals':
     ensure => present,
+    managehome => true,
+    home => /home/fundamentals,
 # password => 'puppet8#labs', # Windows requires a plain text password
 # groups => ['Users'], # Display in Windows Control Panel
+  }
+  file { '/home/fundamentals':
+    ensure => directory,
+    owner => 'fundamentals',
+    group => 'fundamentals',
+    mode => '0755',
   }
 }
