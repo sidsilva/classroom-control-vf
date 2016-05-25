5cfc9dcfa6397d9bc4f67706c105610139300440
sidsilva <sidmikesilva@gmail.com>
2016-05-25 12:01:03 -0600

    Update vhost.pp

--- site/nginx/manifests/vhost.pp
+++ site/nginx/manifests/vhost.pp
@@ -1,7 +1,21 @@
 define nginx::vhost (
   $port       = '80',
   $servername = $title,
-  $docroot    = "${nginx::docroot}/vhosts/$title}",
+  $docroot    = "${nginx::docroot}/vhosts/${title}",
 ) {
-
+  file { $docroot:
+    ensure => direcotry,
+  }
+  
+  file { "$docroot}/index.html":
+    ensure => file,
+    content => template('nginx/index.html.erb')
+  }
+  
+  file { "nginx-vhost-${title}":
+    ensure  => file,
+    path    => "${nginx::blocdir}/${title}.conf",
+    content => template('nginx/vhost.conf.erb'),
+    notify  => Service['nginx'],
+  }
 }
