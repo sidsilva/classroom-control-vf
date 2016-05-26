2812f3eefc799c88536d7b8290668578b4b18e41
sidsilva <sidmikesilva@gmail.com>
2016-05-26 13:37:41 -0600

    Update vhost.pp

--- site/nginx/manifests/vhost.pp
+++ site/nginx/manifests/vhost.pp
@@ -1,21 +1,29 @@
 define nginx::vhost (
-  $port       = '80',
+  $port = '80',
   $servername = $title,
-  $docroot    = "${nginx::docroot}/vhosts/${title}",
+  $docroot = "${nginx::docroot}/vhosts/${title}",
 ) {
-  file { $docroot:
-    ensure => direcotry,
+  File {
+    owner => $nginx::owner,
+    group => $nginx::group,
+    mode => '0664',
   }
-  
+  # Hack to work around lack of DNS. Not for production use!
+  host { $title:
+    ip => $::ipaddress,
+  }
+  file { "nginx-vhost-${title}":
+    ensure => file,
+    path => "${nginx::confdir}/conf.d/${title}.conf",
+    content => template('nginx/vhost.conf.erb'),
+    notify => Service['nginx'],
+  }
+  file { $docroot:
+    ensure => directory,
+    before => File["nginx-vhost-${title}"],
+  }
   file { "${docroot}/index.html":
     ensure => file,
-    content => template('nginx/index.html.erb')
-  }
-  
-  file { "nginx-vhost-${title}":
-    ensure  => file,
-    path    => "${nginx::blocdir}/${title}.conf",
-    content => template('nginx/vhost.conf.erb'),
-    notify  => Service['nginx'],
+    content => template('nginx/index.html.erb'),
   }
 }
